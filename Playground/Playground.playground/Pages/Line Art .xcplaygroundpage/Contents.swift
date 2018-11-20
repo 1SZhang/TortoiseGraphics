8e9752a964c045917393440c3342e0f818b96847
Peter Zhang <szhang@lcs.on.ca>
2018-11-20 12:48:33 -0500

    i got random line form the  top left corner .

--- Playground/Playground.playground/Pages/Line Art .xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Line Art .xcplaygroundpage/Contents.swift
@@ -10,40 +10,30 @@ PlaygroundPage.current.liveView = canvas
 canvas.drawing { turtle in
     
     // Draw 18 small circles rotated around the origin
-    for _ in 1...18 {
-        turtle.right(20)
-        
-        // Draw a small circle
-        for _ in 1...18 {
-            turtle.right(20)
-            turtle.forward(20)
-            
-        }
-    }
-    for _ in 1...100 {
-        
-        // Turn a random amount
-        let turn = turtle.random(360)
-        turtle.left(turn)
-        
-        // Draw a random line length
-        let distance = turtle.random(250)
-        turtle.forward(distance)
-        
-        // Go back to centre of canvas and original heading
+//    for _ in 1...18 {
+//        turtle.right(20)
+//
+//        // Draw a small circle
+//        for _ in 1...18 {
+//            turtle.right(20)
+//            turtle.forward(20)
+//
+//        }
+//    }
+    
+    // Make 25 random line
+    for _ in 1...25 {
+        // Draw radom line from the corner
         turtle.penUp()
-        turtle.goto(0, 0)
-        turtle.setHeading(0)
+        turtle.goto(-250, 250)
         turtle.penDown()
+        turtle.setHeading(0)
         
-    
-    
-    // Hide the turtle when done
-//    turtle.hideTortoise()
+        let randomTurn = turtle.random(90) + 90
+        let randomDistence = turtle.random(240)
+        turtle.right(randomTurn)
+        turtle.forward(randomDistence)
     }
     
-    
-    
-    
-    
+   
 }
