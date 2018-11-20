7c05babdccab6e33f85f4eb3820d9fc6ec6edd30
Peter Zhang <szhang@lcs.on.ca>
2018-11-20 14:23:37 -0500

    Finished circle with  all corner 30 random line

--- Playground/Playground.playground/Pages/Line Art .xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Line Art .xcplaygroundpage/Contents.swift
@@ -10,19 +10,19 @@ PlaygroundPage.current.liveView = canvas
 canvas.drawing { turtle in
     
     // Draw 18 small circles rotated around the origin
-//    for _ in 1...18 {
-//        turtle.right(20)
-//
-//        // Draw a small circle
-//        for _ in 1...18 {
-//            turtle.right(20)
-//            turtle.forward(20)
-//
-//        }
-//    }
+    for _ in 1...18 {
+        turtle.right(20)
+
+        // Draw a small circle
+        for _ in 1...18 {
+            turtle.right(20)
+            turtle.forward(20)
+
+        }
+    }
     
-    // Make 25 random line
-    for _ in 1...25 {
+    // Make 25 random lines from top left
+    for _ in 1...30 {
         // Draw radom line from the corner
         turtle.penUp()
         turtle.goto(-250, 250)
@@ -34,6 +34,46 @@ canvas.drawing { turtle in
         turtle.right(randomTurn)
         turtle.forward(randomDistence)
     }
-    
-   
+
+    // Make 25 random lines from top right
+    for _ in 1...30 {
+        // Draw radom line from the corner
+        turtle.penUp()
+        turtle.goto(250, 250)
+        turtle.penDown()
+        turtle.setHeading(0)
+        
+        let randomTurn = turtle.random(90) + 180
+        let randomDistence = turtle.random(240)
+        turtle.right(randomTurn)
+        turtle.forward(randomDistence)
+    }
+
+    // Make 25 random lines from bottom right
+    for _ in 1...30 {
+        // Draw radom line from the corner
+        turtle.penUp()
+        turtle.goto(250, -250)
+        turtle.penDown()
+        turtle.setHeading(0)
+        
+        let randomTurn = turtle.random(90) + 270
+        let randomDistence = turtle.random(240)
+        turtle.right(randomTurn)
+        turtle.forward(randomDistence)
+    }
+
+    // Make 25 random lines from bottom left
+    for _ in 1...30 {
+        // Draw radom line from the corner
+        turtle.penUp()
+        turtle.goto(-250,-250)
+        turtle.penDown()
+        turtle.setHeading(0)
+        let randomTurn = turtle.random(90) + 0
+        let randomDistence = turtle.random(240)
+        turtle.right(randomTurn)
+        turtle.forward(randomDistence)
+    }
+
 }
